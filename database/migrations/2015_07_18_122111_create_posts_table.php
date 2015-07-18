8a8d501c8713e01d36ca5bec328b88f47e06919f
soywod <clementdouin21@gmail.com>
2015-07-18 14:43:11 +0200

    Create posts migration

---
+++ database/migrations/2015_07_18_122111_create_posts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePostsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable()->unsigned();
+            $table->string('title');
+            $table->string('excerpt', 60);
+            $table->text('content');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->string('thumbnail_link');
+            $table->string('url_site');
+            $table->enum('status', ['published', 'unpublished'])->default('published');
+
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_user_id_foreign');
+        });
+
+        Schema::drop('posts');
+    }
+}
