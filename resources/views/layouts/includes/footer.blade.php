7fe610588d42d1518e9a8bb4388eb1f6e404d3b3
soywod <clementdouin21@gmail.com>
2015-07-19 17:06:52 +0200

    Add footer + menu href

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,1 +1,13 @@
-<h3>FOOTER</h3>
+<footer>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}">accueil</a></li>
+                    <li><a href="{{ url('mentions') }}">mentions légales</a></li>
+                    <li><a href="{{ url('contact') }}">contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</footer>
--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -11,11 +11,11 @@
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="{{ url('/') }}">accueil</a></li>
-                    <li><a href="#">à propos</a></li>
-                    <li><a href="#">contact</a></li>
+                    <li><a href="{{ url('about') }}">à propos</a></li>
+                    <li><a href="{{ url('contact') }}">contact</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">administration</a></li>
+                    <li><a href="{{ url('auth/login') }}">administration</a></li>
                 </ul>
             </div>
         </div>
